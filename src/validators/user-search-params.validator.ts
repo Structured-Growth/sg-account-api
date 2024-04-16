7fa882118fa5aa0b7972de9e9cd5bad275ffcc18
ryzhov <9920529@gmail.com>
2024-04-16 13:18:08 +0300

    fix(SG-6): user search params

--- src/validators/user-search-params.validator.ts
+++ src/validators/user-search-params.validator.ts
@@ -5,7 +5,7 @@ export const UserSearchParamsValidator = joi.object({
 	query: joi
 		.object({
 			orgId: joi.number().positive().required().label("Organization Id"),
-			accountId: joi.number().positive().required().label("Account Id"),
+			accountId: joi.number().positive().label("Account Id"),
 			firstName: joi.array().items(joi.string().max(50).required()).label("First name"),
 			lastName: joi.array().items(joi.string().max(50).required()).label("Last name"),
 			birthday: joi.array().items(joi.date().iso().label("Birthday").required().allow(null)).length(2),
