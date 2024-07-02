bb813350ebedf1385c7bd2bb76d21a61fc084325
anton-devline <anton.z@devline.pro>
2024-07-02 20:32:41 +0100

    fix(SG-215): account api bugs

--- database/migrations/20240702143707-remove-unique-constraint-from-organization-name.js
+++ database/migrations/20240702143707-remove-unique-constraint-from-organization-name.js
@@ -8,14 +8,6 @@ module.exports = {
 		await queryInterface
 			.removeConstraint("organizations", "organizations_name_key")
 			.catch((err) => console.log("Constraint organizations_name_key doesn't exist or already removed"));
-		await queryInterface
-			.removeConstraint("organizations", "organizations_name_key1")
-			.catch((err) => console.log("Constraint organizations_name_key1 doesn't exist or already removed"));
-
-		await queryInterface.changeColumn("organizations", "name", {
-			type: Sequelize.STRING(100),
-			allowNull: false,
-		});
 	},
 
 	async down(queryInterface) {
