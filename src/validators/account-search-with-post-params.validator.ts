f20cdf7bcb59733e5d6c7ae5c0e2845e91838381
anton-devline <anton.z@devline.pro>
2024-10-10 15:57:45 +0100

    fix(SG-316): accounts search

--- src/validators/account-search-with-post-params.validator.ts
+++ src/validators/account-search-with-post-params.validator.ts
@@ -3,11 +3,14 @@ import { CommonSearchParamsValidator } from "./common-search-params.validator";
 
 export const AccountSearchWithPostParamsValidator = joi.object({
 	query: joi.object(),
-	body: joi
-		.object({
-			orgId: joi.number().positive().required().label("Organization Id"),
-			status: joi.array().items(joi.string().valid("active", "inactive", "archived").required().label("Status")),
-			metadata: joi.object().label("Metadata"),
-		})
-		.concat(CommonSearchParamsValidator),
+	body: joi.object({
+		id: joi.array().items(joi.number().positive().required()).label("Entity IDs"),
+		arn: joi.array().items(joi.string().required()).label("Entity ARNs"),
+		page: joi.number().positive().label("Page"),
+		limit: joi.number().positive().label("Limit"),
+		sort: joi.array().items(joi.string().required()).label("Sort"),
+		orgId: joi.number().positive().required().label("Organization Id"),
+		status: joi.array().items(joi.string().valid("active", "inactive", "archived").required().label("Status")),
+		metadata: joi.object().label("Metadata"),
+	}),
 });
