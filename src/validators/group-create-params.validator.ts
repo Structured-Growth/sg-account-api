6fa16640ea323ed20b63c160956dd78ff378b537
Artem Ryzhov <aryzhov@allezhealth.com>
2025-01-22 10:19:33 +0200

    ci(SG-483): fix group creation

--- src/validators/group-create-params.validator.ts
+++ src/validators/group-create-params.validator.ts
@@ -3,6 +3,7 @@ import { joi } from "@structured-growth/microservice-sdk";
 export const GroupCreateParamsValidator = joi.object({
 	query: joi.object(),
 	body: joi.object({
+		orgId: joi.number().positive().label("Organization Id"),
 		accountId: joi.number().positive().required().label("Account Id"),
 		parentGroupId: joi.number().positive().label("Parent group Id"),
 		title: joi.string().min(2).max(50).required().label("Group title"),
