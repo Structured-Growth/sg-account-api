219e0748866e31b1d154265e5b198b40e8224709
anton-devline <anton.z@devline.pro>
2025-04-23 21:35:14 +0100

    feat(SG-597): Localization of all APIs

--- src/validators/preferences-update-params.validator.ts
+++ src/validators/preferences-update-params.validator.ts
@@ -12,6 +12,6 @@ export const PreferencesUpdateParamsValidator = joi.object({
 				locale: joi.string().valid("US", "UA").label("validator.preferences.locale"),
 			})
 			.label("Preferences"),
-		metadata: joi.object().label("validator.preferences.metadata"),
+		metadata: joi.object().unknown().label("validator.preferences.metadata"),
 	}),
 });
